010a89c972c22b56a0339852d9181d2d8ec8f048
rahulaunni <rahulaunni@gmail.com>
2018-01-30 17:41:14 +0530

    electron added and edited .package start, added code for window creation

--- server.js
+++ server.js
@@ -40,6 +40,23 @@ var Infusionhistory = require('./models/infusionhistories');
 //for logging requests
 app.use(morgan('dev'));
 
+//electron window
+const electron = require('electron')
+const {BrowserWindow} = electron;
+var mainWindow = null;
+function createWindow () {
+  // Create the browser window.
+  win = new BrowserWindow({width: 800, height: 600})
+
+  // and load the index.html of the app.
+  win.loadURL(url.format({
+    pathname: path.join(__dirname,'public/app/views/','index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+}
+
+
 //for updating app
 app.post('/admin/update',function (req,res) {
     require('simple-git')()
